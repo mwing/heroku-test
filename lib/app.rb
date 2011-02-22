13a27a6247a4186aa9768df8018e72dce578b3e8
Miso Korkiakoski <miso.korkiakoski@reaktor.fi>
2011-02-22 22:15:56 +0200

    add mongo conf for MongoHQ in heroku

--- lib/app.rb
+++ lib/app.rb
@@ -2,8 +2,11 @@ require 'rubygems'
 #require 'bundler'
 #Bundler.setup
 require 'sinatra/base'
+require 'mongo_mapper'
+
 class MyApp < Sinatra::Base
 
+MongoMapper.connection = Mongo::Connection.from_uri(ENV["MONGOHQ_URL"]) if ENV["MONGOHQ_URL"]
   get '/' do
     "Hello world, it's #{Time.now} at the server!"
   end
