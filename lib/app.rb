47c2a6a44b32f1bb39041e7e03b0b5869bc3f819
Miso Korkiakoski <miso.korkiakoski@reaktor.fi>
2011-02-22 22:24:15 +0200

    set mongo connection

--- lib/app.rb
+++ lib/app.rb
@@ -4,10 +4,9 @@ require 'rubygems'
 require 'sinatra/base'
 require 'mongo_mapper'
 
+MongoMapper.connection = Mongo::Connection.from_uri(ENV["MONGOHQ_URL"]) if ENV["MONGOHQ_URL"]
+
 class MyApp < Sinatra::Base
-
-#MongoMapper.connection = Mongo::Connection.from_uri(ENV["MONGOHQ_URL"]) if ENV["MONGOHQ_URL"]
-
   get '/' do
     "Hello world, it's #{Time.now} at the server! also, url: #{ENV['MONGOHQ_URL']}"
   end
