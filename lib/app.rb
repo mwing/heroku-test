a91e32123e241a72c436cd9e2ee8b34701135b68
Miso Korkiakoski <miso.korkiakoski@reaktor.fi>
2011-02-22 22:40:05 +0200

    add couple of methods

--- lib/app.rb
+++ lib/app.rb
@@ -12,8 +12,22 @@ class MyApp < Sinatra::Base
     "Hello world, it's #{Time.now} at the server!"
   end
   
+  #get for now, can't be arsed to do a form for post
   get "/add/:item" do
-    item = Item.create(:item => params[:item])
+    item = Item.new(:item => params[:item])
+    item.save # Heh, 3 lines for a one-liner :)
     item.id
+  end
+  
+  get "/list" do
+    Item.all.to_json
+  end
+  
+  get "/delete/:id" do
+    Item.find_by_id(params[:id]).delete
+  end
+  
+  get "/count" do
+    Item.all.count.to_s
   end
 end
