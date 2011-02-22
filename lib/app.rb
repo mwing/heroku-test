65fbd54c1240f8fca4e9771fc85d8276f1e2364a
Miso Korkiakoski <miso.korkiakoski@reaktor.fi>
2011-02-22 21:48:07 +0200

    make rack app

--- lib/app.rb
+++ lib/app.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup
 
-require 'sinatra'
+require 'sinatra/base'
+class MyApp < Sinatra::Base
 
-get '/' do
-  "Hello world, it's #{Time.now} at the server!"
+  get '/' do
+    "Hello world, it's #{Time.now} at the server!"
+  end
 end
